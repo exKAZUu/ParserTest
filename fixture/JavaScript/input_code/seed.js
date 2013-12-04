b50a92412a727887d99421998849a8730a62cf50
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-12-04 17:27:11 +0900

    Enhance seed code.

---
+++ fixture/JavaScript/input_code/seed.js
@@ -0,0 +1,26 @@
+console.log(args != null);
+
+for (var i = 0; i < args.length; i++) {
+	console.log(1);
+	for (var j = 0; args.length == 0; j++) {
+		console.log("abc");
+	}
+}
+
+while (args.length == 0) {
+	while (args.length < 0) {
+		i++;
+	}
+}
+
+do {
+	do {
+		i = 0;
+	} while (args.length < 0);
+} while (args.length == 0);
+
+if (args.length < 0) {
+	if (args.length == 0) {
+		func();
+	}
+}
