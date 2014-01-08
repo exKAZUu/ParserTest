d5a894db38eba0cdebbed1899de63ade4a7114b0
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-01-08 16:24:28 +0900

    Improve seed for Lua.

---
+++ fixture/Lua/input_code/Seed.lua
@@ -0,0 +1,24 @@
+i = 0
+f()
+
+if i == 0 then
+	if j ~= 0 then
+	elseif 2 > j then
+	end
+elseif 1 < i then
+end
+
+while i ~= 0 do
+	while j == 0 do
+	end
+end
+
+repeat
+	repeat
+	until j ~= 0
+until i == 0
+
+for i = 0, 1, 1 do
+	for j = 2, 3, 4 do
+	end
+end
