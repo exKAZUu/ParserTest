1b2eb67a7d2782d79d2dbb9304f970591d016495
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-01-14 11:17:05 +0900

    Add a new seed file.

---
+++ fixture/Java/input_code/Seed2.java
@@ -0,0 +1,23 @@
+public class Test {
+    private final int VERSION = 1;
+
+    public void grow(int a, int b) {
+        ;
+        {if(a>b){
+            b++;
+        }}
+        int result;
+        LABEL:
+          result = a + b * a;
+        switch(result){
+            case 10:
+            case 11:
+            case 12:
+              {}
+              result++;
+              break;
+            default:
+              result = 0;
+        }
+    }
+}
