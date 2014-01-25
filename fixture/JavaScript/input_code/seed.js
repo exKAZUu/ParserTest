7d426b27e005dadcf30de0391f636a68581ff3e2
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-01-25 14:05:14 +0900

    Improve seed source code.

--- fixture/JavaScript/input_code/seed.js
+++ fixture/JavaScript/input_code/seed.js
@@ -2,31 +2,21 @@ console.log(args != null);
 
 for (var i = 0; i < args.length; i++) {
 	console.log(1);
-	for (; true; ) {
-	}
 }
 
 TEST:
 while (args.length == 0) {
-	while (true) {
-	}
 }
 
 do {
-TEST2:
-	do {
-	} while (true);
 } while (args.length == 0);
 
 if (args.length < 0) {
-	if (true) {
-	}
-	func();
-	;
 }
 	
 {}
 { i = i - 1; }
 ;
 i = i + 1;
+TEST2:
 func();
--- fixture/Php/input_code/Seed.php
+++ fixture/Php/input_code/Seed.php
@@ -7,27 +7,18 @@ $i = 0;
 f();
 
 if ($i == 0) {
-	if ($j != 0) {
-	} elseif (2 > $j) {
-	}
 } elseif (1 < $i) {
 }
 else {
 }
 
 while ($i != 0) {
-	while ($j == 0) {
-	}
 }
 
 do {
-	do {
-	} while ($j == 0);
 } while ($i != 0);
 
 for ($i = 0; $i < 10; $i++) {
-	for ($j = 0; $j != 5; $j++) {
-	}
 }
 
 {}
